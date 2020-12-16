e714d7eacb1311c020963fd79b9425d12eb78c68
Tobias Tögel <71888952+TobiTgl@users.noreply.github.com>
2020-12-16 21:56:14 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ app.get('/', (req, res) => {
 
 app.get('/user', passport.authenticate('basic', { session: false }), (req, res) => {
   pool.query('SELECT * FROM users WHERE id=$1',[req.user.id]).then(results => {
+    console.log(req)
+    console.log(results)
         res.json(results.rows);  
         res.sendStatus(200);          
   })
